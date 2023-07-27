83e6b7a52c230f7affb6453665c12b5f01c646b0
KAMAL <kamaltejp@gmail.com>
2023-07-27 18:47:10 +0530

    updated cv

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -34,7 +34,7 @@ const SideBar = () => {
                 <a
                   target="_blank"
                   rel="noopener noreferrer"
-                  href="https://drive.google.com/file/d/18FX91qOHYzgyGdUqHYWo-aNUIE5rDQhi/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1Z1m6U9VbSff5AhoTUgf1xcxUP9tTAs-1/view?usp=sharing"
                 >
                   cv
                 </a>
