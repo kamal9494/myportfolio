b7be7a2f842158d816e459da3275f62d9043668c
KAMAL <kamaltejp@gmail.com>
2023-12-27 16:42:37 +0530

    added project

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,30 +23,98 @@ export default function Home() {
             <div className={styles.content}>
               <label className={styles.SecTitle}>Intro</label>
               <p className={styles.about}>
-                I am a student at Vellore Institute of Technology, Amaravati, 
-                currently pursuing the final year of a Bachelor of Engineering 
+                I am a student at Vellore Institute of Technology, Amaravati,
+                currently pursuing the final year of a Bachelor of Engineering
                 in Computer Science.
               </p>
               <p className={styles.about}>
-                My interests are web
-                development and problem solving.
+                My interests are web development and problem solving.
               </p>
             </div>
             <div className={styles.content}>
               <div className={styles.SecTitleWrap}>
                 <h2 className={styles.SecTitle}>Projects</h2>
-                <button type="button" onClick={() => toProjects()}>Browse all</button>
+                <button type="button" onClick={() => toProjects()}>
+                  Browse all
+                </button>
               </div>
-              <div className={styles.project_title}>
-                <span
-                  className={styles.project_btn}
-                  onClick={() => toChatBot()}
-                >
-                  ChatBot Web Application using AWS Amplify as backend
-                </span>
-                <a className={styles.live} rel="noreferrer" target="_blank" href="https://academicbot.netlify.app">live demo</a>
+              <div>
+                <div className={styles.project_title}>
+                  <span
+                    className={styles.project_btn}
+                    onClick={() => toChatBot()}
+                  >
+                    ChatBot Web Application using AWS Amplify as backend
+                  </span>
+                  <a
+                    className={styles.live}
+                    rel="noreferrer"
+                    target="_blank"
+                    href="https://academicbot.netlify.app"
+                  >
+                    live demo
+                  </a>
+                  <a
+                    className={styles.github}
+                    rel="noreferrer"
+                    target="_blank"
+                    href="https://github.com/kamal9494/acadChatbot"
+                  >
+                    github
+                  </a>
+                </div>
+                <div className={styles.tech}>
+                  <span>Tech Stack:</span>
+                  <p>React.js, CSS, AWS Amplify, Amazon Lex</p>
+                </div>
+                <ul className="uolist">
+                  <li className={styles.bullet}>
+                    developed a chatbot web app to address university
+                    course-related queries.
+                  </li>
+                </ul>
               </div>
-              <p className={styles.bullet}><i>developed a chatbot web app to address university course-related queries.</i></p>
+
+              <div>
+                <div className={styles.project_title}>
+                  <span
+                    className={styles.project_btn}
+                    // onClick={() => toChatBot()}
+                  >
+                    Sport Inventory Management System
+                  </span>
+                  <a
+                    className={styles.live}
+                    rel="noreferrer"
+                    target="_blank"
+                    href="https://sporttrackpro.vercel.app/"
+                  >
+                    live demo
+                  </a>
+                  <a
+                    className={styles.github}
+                    rel="noreferrer"
+                    target="_blank"
+                    href="https://github.com/kamal9494/sportenvbackend"
+                  >
+                    github
+                  </a>
+                </div>
+                <div className={styles.tech}>
+                  <span>Tech Stack:</span>
+                  <p>MongoDB, Express.js, React.js, Node.js, TailwindCSS</p>
+                </div>
+                <ul className="uolist">
+                  <li className={styles.bullet}>
+                    In this project students can easily borrow,
+                    return, and request sports gear through the site.
+                  </li>
+                  <li className={styles.bullet}>
+                    Admins have a control panel to track student actions and
+                    monitor the inventory.
+                  </li>
+                </ul>
+              </div>
             </div>
             <div className={styles.dash}>
               <div>
